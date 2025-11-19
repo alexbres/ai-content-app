45ccb6424b2608a813be4e7b95bceb03032a8845
alexbres <alexbres@gmail.com>
2025-11-19 19:44:06 +0300

    Update package dependencies for TypeScript and ESLint. Add @typescript-eslint/eslint-plugin and @typescript-eslint/parser at version 8.15.0, and include eslint-plugin-react-hooks at version 5.1.0-rc.0. Update package-lock.json to reflect these changes.

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-empty': 'off',
+    'no-console': 'off',
+  },
+}
+
+
